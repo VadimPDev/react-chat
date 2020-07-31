138f1ae275c0a1717a223cd27f814cabb355faf2
Vadim <denba2013@yandex.ru>
2020-07-31 14:08:38 +0300

    first commit

---
+++ users.js
@@ -0,0 +1,32 @@
+const users = []
+
+const addUser = ({id,name,room}) =>{
+    name = name.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    const existingUser = users.find(u => u.room === room && u.name === name)
+    
+    if(existingUser){
+        return { error:'Username is taken'}
+    }
+
+    const user = {id,name,room}
+    users.push(user)
+    return {user}
+}
+
+const removeUser = (id) =>{
+
+    const index = users.findIndex(u => u.id === id)
+
+    if(index !== -1){
+        return users.splice(index,1)[0]
+    }
+
+}
+
+const getUser =(id) => users.find(u => u.id === id)
+
+const getUserInRoom = (room) => users.filter(u => u.room === room)
+
+module.exports = {addUser,removeUser,getUser,getUserInRoom}
